19f8a4c195837c26d1ebbb7f448d8fdb03b6f1a1
vinayak irabatti <vinayakirabatti22@gmail.com>
2022-03-20 23:16:12 +0530

    second day of course

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,19 @@
-def solve():
-    return "hello world"
+from itertools import permutations
 
-print(solve())
+def solve(N,M):
+    dp = permutations("123456789",N)
+
+    c = 0
+    for i in dp:
+        temp =""
+        for subb in i:
+            temp += (str(subb)*2)
+        if int(temp)%M == 0:
+            c += 1
+    return c
+
+
+    
+
+
+print(solve(1,7))
